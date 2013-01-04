a452433f9fbf30d0b01e73ad2e746607f0a771c6
John Keech <jkeech@live.com>
2013-01-03 23:24:31 -0600

    fixed js date parsing error

--- js/luminarium.js
+++ js/luminarium.js
@@ -182,18 +182,18 @@ function prettyDate(date_str){
         return "Unknown";
         
     switch(parseInt(date_parts[1])){
-        case 0: result = "January"; break;
-        case 1: result = "February"; break;
-        case 2: result = "March"; break;
-        case 3: result = "April"; break;
-        case 4: result = "May"; break;
-        case 5: result = "June"; break;
-        case 6: result = "July"; break;
-        case 7: result = "August"; break;
-        case 8: result = "September"; break;
-        case 9: result = "October"; break;
-        case 10: result = "November"; break;
-        case 11: result = "December"; break;
+        case 1: result = "January"; break;
+        case 2: result = "February"; break;
+        case 3: result = "March"; break;
+        case 4: result = "April"; break;
+        case 5: result = "May"; break;
+        case 6: result = "June"; break;
+        case 7: result = "July"; break;
+        case 8: result = "August"; break;
+        case 9: result = "September"; break;
+        case 10: result = "October"; break;
+        case 11: result = "November"; break;
+        case 12: result = "December"; break;
         default: return "Unknown";
     }
     
