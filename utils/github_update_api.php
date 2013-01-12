6d7ecf424758c167ce146385fbd199377e02f1b7
John Keech <jkeech@live.com>
2013-01-11 23:50:47 -0600

    added post hook for api repo

---
+++ utils/github_update_api.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Hook GitHub Post-Receive 
+ *
+ * @copyright (c)2005-2011, WDT Media INC (http://wdtmedia.com)
+ * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @link http://github.com/jadb/hook-github-post-receive
+ * @author jadb
+ *
+ * Modified by John Keech for use with The Luminarium
+ *
+ */
+ 
+if (!array_key_exists('payload', $_POST))
+	return;
+
+extract(json_decode(stripslashes($_POST['payload']), true));
+
+if (!isset($repository) || !isset($repository['name']))
+	return;
+
+// perform the git pull
+$loc = '/home/warlord/www/api';
+$result = shell_exec('cd ' . $loc . ' && git pull');
+
+// log results to file to later viewing and debugging
+$filename = '/home/warlord/logs/github_pull.log';
+$fd = fopen($filename, 'a');
+fwrite($fd, $result . "\n");
+fclose($fd);
+?>
